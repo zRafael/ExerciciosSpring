4ece6ab40d87930f95d17ec0adb3ac876c57d922
zRafael <rafael.silva@bandtec.com.br>
2022-05-05 14:34:40 -0300

    Adicionando projeto Loja de Games Spring

---
+++ lojagames/src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,77 @@
+package com.generation.lojagames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+
+	@NotNull
+	@Size(min = 5, max = 100, message = "O campo deve ter no mínimo 5 caracteres, e no máximo 100")
+	public String nome;
+
+	@NotNull
+	public Double preco;
+
+	@NotNull
+	public String descricao;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+
+}
