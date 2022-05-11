fe965c05abfa44c2afe66a2d2fd2e1759367de65
zRafael <rafael.silva@bandtec.com.br>
2022-05-11 16:00:39 -0300

    Adicionando Put e Delete

--- lojagames/src/main/java/com/generation/lojagames/controller/UsuarioController.java
+++ lojagames/src/main/java/com/generation/lojagames/controller/UsuarioController.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,5 +53,15 @@ public class UsuarioController {
 		return usuarioService.cadastraUsuario(usuario)
 				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+	}
+	
+	@PutMapping
+	public ResponseEntity<Usuario> put (@RequestBody Usuario usuario){
+		return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuario));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		usuarioRepository.deleteById(id);
 	}
 }
